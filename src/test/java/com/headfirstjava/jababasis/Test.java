49cdf2e3fbcbc810a96dab0c4761bbaf0aed80fc
liuyuhang <962653483@qq.com>
2020-11-04 13:52:16 +0800

    test(test): nullPointerException demo
    空指针异常 demo 测试

---
+++ src/test/java/com/headfirstjava/jababasis/Test.java
@@ -0,0 +1,24 @@
+package com.headfirstjava.jababasis;
+
+
+/**
+ * @author: liuyuhang
+ * @date: 2020/11/04
+ */
+public class Test {
+
+
+    /**
+     * nullPointerException
+     * 一般会去使用一个一定不为空的字符串来
+     * 比较另外一个传递过来的字符串来避免
+     * 空指针异常
+     */
+    @org.junit.jupiter.api.Test
+    public void nullPointerTest(){
+        String a = null;
+        String b = "1";
+
+        b.equals(a);
+    }
+}
