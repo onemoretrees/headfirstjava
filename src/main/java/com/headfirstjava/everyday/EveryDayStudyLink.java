a76f539ad0acf07b93ef45a4cedaaaea8ccd8eae
liuyuhang <962653483@qq.com>
2020-11-05 11:26:07 +0800

    feat(doc): 这里面存储一些每天学到的新的并且需要花时间进一步整理的知识
    这里面存储一些每天学到的新的并且需要花时间进一步整理的知识
    
    Closes none

---
+++ src/main/java/com/headfirstjava/everyday/EveryDayStudyLink.java
@@ -0,0 +1,116 @@
+package com.headfirstjava.everyday;
+
+/**
+ * @author: liuyuhang
+ * @date: 2020/11/05
+ */
+public class EveryDayStudyLink {
+    /**
+     * 这里面存储一些每天学到的新的并且需要花时间进一步整理的知识
+     *
+     */
+
+
+    /**
+     *
+     * git merge 和 git rebase 小结
+     * https://www.cnblogs.com/veryvalley/p/8026580.html
+     *
+     *
+     */
+
+
+    /**
+     *
+     * idea 注入数据提示 Field injection is not recommended
+     * https://blog.csdn.net/Li_Ya_Fei/article/details/104448398?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.channel_param
+     *
+     *
+     */
+
+    /**
+     *
+     * mybatis eq 用法
+     * https://blog.csdn.net/qq_41115103/article/details/107536997
+     */
+
+
+    /**
+     *
+     * git 推送分支
+     *
+     * https://www.cnblogs.com/everfight/p/git_push_local_branch.html
+     *
+     * 如果你本地的分支名称和远程名称相同，则使用以下命令：
+     *
+     * git push origin branchName
+     * 如果本地的分支跟远程的分支不一样，则指定本地和远程的分支的名称：
+     *
+     * git push origin localBranchName:remoteBranchName
+     *
+     *
+     * git 分支查看与切换
+     * 1.查看所有分支
+     * git branch -a
+     *
+     * 2.查看当前使用分支(结果列表中前面标*号的表示当前使用分支)
+     * git branch
+     *
+     * 3.切换分支
+     * git checkout 分支名
+     *
+     */
+
+
+    /**
+     *
+     * sourcetree 分支使用
+     * https://www.jianshu.com/p/ea9ebad692d2
+     *
+     */
+
+
+    /**
+     *
+     * jakson 使用
+     * https://www.cnblogs.com/guanbin-529/p/11488869.html
+     *
+     */
+
+
+    /**
+     *
+     * mybatis 配置文件 头文件报红
+     * https://blog.csdn.net/qq_29564213/article/details/88753292
+     *
+     */
+
+
+    /**
+     *
+     * postMan 传 map  参数
+     * https://blog.csdn.net/weixin_41288824/article/details/104810188
+     *
+     */
+
+    /**
+     *
+     * idea 关闭 xml 文件黄色背景
+     * https://blog.csdn.net/u010318957/article/details/72459183
+     *
+     */
+
+    /**
+     * 常见数据库连接地址代码
+     * https://www.cnblogs.com/activiti/p/6703478.html
+     *
+     */
+
+
+    /**
+     *
+     *nacos 服务注册demo
+     * https://blog.csdn.net/xxh_1229/article/details/108813542
+     *
+     */
+}
